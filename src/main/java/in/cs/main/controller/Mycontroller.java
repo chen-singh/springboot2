55ac8c6a79accaf0d87184c6f07faa29096223de
chen-singh <cssodho123@gmail.com>
2026-05-16 00:03:47 +0400

    add updates

--- src/main/java/in/cs/main/controller/Mycontroller.java
+++ src/main/java/in/cs/main/controller/Mycontroller.java
@@ -2,12 +2,15 @@ package in.cs.main.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class Mycontroller {
 
 	@GetMapping("/")
+	@ResponseBody
 	public String  hello() {
+	
 		return  "hello chen";
 				
 				
