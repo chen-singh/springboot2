a6c3052323128afb94b504a7978a0595ab5fc391
chen-singh <cssodho123@gmail.com>
2026-05-15 23:57:35 +0400

    first commit

--- src/main/java/in/cs/main/controller/Mycontroller.java
+++ src/main/java/in/cs/main/controller/Mycontroller.java
@@ -1,5 +1,17 @@
 package in.cs.main.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class Mycontroller {
 
+	@GetMapping("/")
+	public String  hello() {
+		return  "hello chen";
+				
+				
+				
+				
+	}
 }
