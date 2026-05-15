c8084fd3c3a2e108268b11c8f7c582d38dc953c1
chen-singh <cssodho123@gmail.com>
2026-05-15 23:49:06 +0400

    first commit

---
+++ src/main/java/in/cs/main/Springboot2Application.java
@@ -0,0 +1,13 @@
+package in.cs.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot2Application.class, args);
+        System.out.print("chen");
+    }
+}
---
+++ src/main/java/in/cs/main/controller/Mycontroller.java
@@ -0,0 +1,5 @@
+package in.cs.main.controller;
+
+public class Mycontroller {
+
+}
